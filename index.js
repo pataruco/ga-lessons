e951c9c401e225997d8b291f3add405059a8d40c
Pedro Martin <pataruco@gmail.com>
2018-11-03 19:59:13 +0000

    Create an index for slides

---
+++ index.js
@@ -0,0 +1,54 @@
+/* 
+fewdLessons
+
+1 html
+2 htmlCss
+3 boxModel
+4 cssSelectors
+5 flexbox
+6 positioning
+7 responsive
+8
+9 js
+10 jquery
+11 jquery-plugins
+12 forms
+13 animations
+14 svg 
+15 accesability
+16 git / sass 
+*/
+
+const fewdLessons = {
+  accesability: 'accesability/accesability.md',
+  animations: 'animations/animations.md',
+  boxModel: 'box-model/box-model.md',
+  cssSelectors: 'css-selectors/css-selectors.md',
+  finalPresentations: 'final-presentations/slides.md',
+  flexbox: 'flexbox/flexbox.md',
+  forms: 'forms/forms.md',
+  html: 'intro-html/html.md',
+  htmlCss: 'html-css/html-css.md',
+  jquery: 'intro-jquery/slides.md',
+  jqueryPlugins: 'intro-jquery-plugins/slides.md',
+  js: 'intro-js/slides.md',
+  positioning: 'positioning/positioning.md',
+  responsive: 'responsive/responsive.md',
+  svg: 'svg/svg.md',
+};
+
+/* 
+introToCode
+
+1 introToCode
+
+*/
+
+const introToCode = {
+  introToCode: '../slides/intro-to-code/intro-to-code.md',
+};
+
+export default {
+  fewdLessons,
+  introToCode,
+};
